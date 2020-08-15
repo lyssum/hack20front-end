ad89c6d558f23affa73ddf3a74813826ac57c8fe
Larry-Tian <ganlin@uw.edu>
2020-08-15 16:59:14 -0700

    Finished setting up log in

--- hack20site/src/App.js
+++ hack20site/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Layout from "./Containers/Layout/Layout"
 import Cards from "./Components/Cards/Cards";
+// Import the user LogIn Page
+import UserLogIn from "./userLogIn"
 import './App.css';
+
+// Import Firebase
+import firebase from 'firebase/app';
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Cards/>
+        <UserLogIn/>
       </Layout>
     </div>
   );
